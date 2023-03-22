2c467bfb8decfbf7a79ddba94f998a9b49012b1d
ella7d <ella.debesai@gmail.com>
2023-03-22 14:24:11 -0700

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.place import place_amenity
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
-from sqlalchemy.orm import relationship
+from models import storage_type
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    """ Class attribute place_amenities must represent a relationship
-    Many-To-Many between the class Place and Amenity. Please see below more
-    detail: place_amenity in the Place update """
-    place_amenities = relationship("Place", secondary='place_amenity',
-                                   back_populates="_amenities")
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
