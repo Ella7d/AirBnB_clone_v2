f6d4d17a8173808699e46b0d2edd38a8cf6c8c20
root <ella.debesai@gmail.com>
2023-04-23 12:08:28 -0700

    fix

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+""" Starts a Flask web app and fetches data fromstorage engine """
 from flask import Flask, render_template
-from models import storage
-from models.state import State
-from models.amenity import Amenity
+from models import storage, State, Amenity
 
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def close_session(foo):
+    """ Closes Session """
     storage.close()
 
 
-@app.route('/hbnb_filters/', strict_slashes=False)
-def display_html():
-    """ Function called with /states route """
+@app.route('/hbnb_filters', strict_slashes=False)
+def states_state(id=None):
+    """ Displays HTML page """
     states = storage.all(State)
     amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', **locals())
 
-    return render_template('10-hbnb_filters.html',
-                           states=states.values(),
-                           amenities=amenities.values())
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    storage.reload()
+    app.run("0.0.0.0", 5000)
