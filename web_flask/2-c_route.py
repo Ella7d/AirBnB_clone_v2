f0d384aa90a643a0eb5c9a56653064236f1decbc
root <ella.debesai@gmail.com>
2023-04-23 12:03:32 -0700

    fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+""" Script that starts a Flask web application """
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Function called with / route """
+    """ Handles the root url """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Function called with /hbnb route """
+    """ Handles hbnb route """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ Function called with /c/<text> route """
-    return 'C %s' % text.replace('_', ' ')
+    """ Handles /c/<text> route """
+    return 'C {}'.format(text.replace('_', ' '))
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
 
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
